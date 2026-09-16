4016c65441fcbd3104fa8c777c2c8e480be64d7f
Zackery Savio <savioz7563@student.faytechcc.edu>
2026-09-16 15:47:48 +0000

    work baybyyyy

--- M2/M2HW1_Savio.cpp
+++ M2/M2HW1_Savio.cpp
@@ -31,19 +31,39 @@ int main() {
 }
 
 void question1() {
+    cout << "       Question 1:" << endl;
     cout << "Write a program that simulates banking transactions" << endl;
+    cout << endl;
+    cout << "       Answer 1:" << endl;
+    cout << 
+    // Setup Strings and integers
+
+    // 
+
 }
 
 void question2() {
-    cout << "General Crates has found that the updated cost per cubic foot for storage is now 0.3, due to economic fluctuations." << endl;
-Management has determined that they cannot currently raise their charge per cubic foot above 0.52 without losing customers.
-Modify the program to match these new conditions
+    cout << "       Question 2:" << endl;
+    cout << "General Crates has found that the updated cost per cubic foot for storage is now 0.3, due to economic fluctuations. " << endl;
+    cout << "Management has determined that they cannot currently raise their charge per cubic foot above 0.52 without losing customers. " << endl;
+    cout << "Modify the program to match these new conditions" << endl;
+    cout << endl;
+    cout << "       Answer 2:" << endl;
 }
 
 void question3() {
-    cout << "Question 3 goes here" << endl;
+    cout << "       Question 3:" << endl;
+    cout << "The program should ask how many pizzas you order, how many slices per pizza, and how many visitors are coming. " << endl;
+    cout << "The program should calculate and display how many pieces of pizza are left over. " << endl;
+    cout << endl;
+    cout << "       Answer 3:" << endl;
+
+
 }
 
 void question4() {
-    cout << "Question 4 goes here" << endl;
+    cout << "       Question 4:" << endl;
+    cout << "Since FTCC has sports teams, you've been hired to write a cheering program." << endl;
+    cout << endl;
+    cout << "       Answer 4:" << endl;
 }
