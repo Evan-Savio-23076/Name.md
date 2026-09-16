e354abc81ba10fd0a42a9c8be66bd0b15d09f053
Zackery Savio <savioz7563@student.faytechcc.edu>
2026-09-16 14:11:07 +0000

    HONEWORK

---
+++ M1/M1HW1_Savio.cpp
@@ -0,0 +1,21 @@
+// CSC 134
+// M2LAB1
+// Savio, Z
+// 9/9/26
+// C++ Homework, that i am REALLY late on doing
+
+#include <iostream>
+
+int main() {
+    // Variables
+    string movie_name = "Interstellar";
+    string movie_director = "Christopher Nolan";
+    int movie_year = "2014";
+    double millions_made = "783"
+
+    //Output
+    cout <<"============================"
+    cout >>"=       Interstellar        "  
+
+
+}
